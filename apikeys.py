775ef7e8b7527686c64f5e92b497a9b6c06b8522
ashley-ngyn <adtnguyen@ucdavis.edu>
2024-02-07 18:24:01 -0800

    delete apikeys.py and config.py

--- apikeys.py
+++
@@ -1,3 +0,0 @@
-weather_key = "254cb374ebd385859897bf560673528e"
-tomorrow_key = "ges6BWcF39MCKORcGw5ZWkGcTvzIF35v"
-nps_key = "Wen4UHsG6Blpz3RvBcBIdoVtuH5uEq9WfX0TEomR"
--- config.py
+++
@@ -1,1 +0,0 @@
-nps_api_key = 'lH40Rhrq9DVAdflevyk90GEJPwJ1EAK2f46gduYx'
